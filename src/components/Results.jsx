61a84500dba1bebfdd616edf67679f219a40784d
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2021-05-09 10:51:35 -0400

    add top 5 programs

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -34,6 +34,20 @@ class Results extends React.Component {
                 [element.latest.cost.tuition.out_of_state, "Out-of-State Tuition"],
             ]
 
+            const programs = element.latest.academics.program_percentage;
+            const sortable = []
+            for(let prog in programs) sortable.push([prog, programs[prog]])
+            console.log(sortable.sort((e1, e2) => e2[1] - e1[1]))
+
+            const rendered = []
+            for(let i = 0; i < 5; i++) rendered.push(sortable[i])
+
+            rendered.map(element => {
+                element[0] = element[0].replaceAll("_", " ")
+                element[0] = element[0].charAt(0).toUpperCase() + element[0].slice(1)
+                return element
+            })
+
             return (
             <center>
             <div className="card border-info mb-3" style={{maxWidth: "70%"}}>
@@ -41,6 +55,12 @@ class Results extends React.Component {
             <div className="card-body">
                <h5 className="card-title" key={index}>{element.school.name} </h5>
                 {stuff.map((element, index) => element[0] && <div className="card-text" key={index}>{element[1] + ": " + element[0]}</div>)}
+                
+                {rendered[0][1] && <><hr /><p><h6>Top 5 programs:</h6>
+                    <div>
+                        {rendered.map((el, ind) => <li key={ind}>{el[0]}: {(el[1] * 100).toFixed(2)}%</li>)}
+                    </div>
+                </p></>}
             </div>
             </div>
             </center>)
