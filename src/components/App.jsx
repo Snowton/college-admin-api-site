a4b899ee84352e5cbb4168457f93ab99a0ef3f72
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2021-05-09 00:52:42 -0400

    make updating better

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,25 @@ import Input from "./Input.jsx"
 const {useState} = React
 
 const App = () => {
-    const [query, setQuery] = useState("New York")
+    const [message, setMessage] = useState([])
 
     const queryChange = (q) => {
         console.log(q.target.value)
-        setQuery(old => q.target.value)
+
+        fetch("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=rYi5aVIgo1ipZMa10ptOzIxlTGZxN2FIcOeE0wjo&school.name=" + q.target.value)
+        .then(res => res.json())
+        .then(({results}) => {
+            console.log(results);
+            if(results) setMessage(old => results)
+        })
+
+        // setMessage(old => q.target.value)
     }
 
     return (<>
         <h1>Hi</h1>
         <Input inputChange={queryChange}></Input>
-        <Results query={query}></Results>
+        <Results message={message}></Results>
     </>)
 }
 
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,27 +4,27 @@ class Results extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {
-            message: []
-        }
+        // this.state = {
+        //     message: []
+        // }
     }
 
-    shouldComponentUpdate(props, state) {
-        console.log(this.props.query != props.query, this.state.message != state.message)
-        return true
-    }
+    // shouldComponentUpdate(props, state) {
+    //     // console.log(this.props.query != props.query, this.state.message != state.message)
+    //     return true
+    // }
 
-    componentDidUpdate() {
-        fetch("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=S4QswG1yOcPueT1ki5kqvg9Kx4YF1VOAudYcz0cQ&school.name=" + this.props.query)
-        .then(res => res.json())
-        .then(({results}) => {
-            this.setState(old => ({message: results}))
-            console.log(results);
-        })
-    }
+    // componentDidUpdate() {
+    //     fetch("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=rYi5aVIgo1ipZMa10ptOzIxlTGZxN2FIcOeE0wjo&school.name=" + this.props.message)
+    //     .then(res => res.json())
+    //     .then(({results}) => {
+    //         this.setState(old => ({message: results}))
+    //         console.log(results);
+    //     })
+    // }
 
     render() {
-        return <ul>{this.state.message.map((element, index) => {
+        return <ul>{this.props.message.map((element, index) => {
             const stuff = [
                 [element.latest.admissions.admission_rate.overall, "Admissions rate"]
             ]
